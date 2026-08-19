61695cf119b15d853389b9db3d3ed74b6e638f3e
Nicole <nicole.herbas@ucb.edu.bo>
2026-08-18 22:03:24 -0400

    feat: multiplication

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,9 @@ import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const sumarButton = document.querySelector("#sumar-button");
+const first1 = document.querySelector("#primer-numero1");
+const second1 = document.querySelector("#segundo-numero1");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 const multiplicarButton = document.querySelector("#multiplicar-button");
@@ -20,8 +23,8 @@ form.addEventListener("submit", (event) => {
 multiplicarButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(first1.value);
+  const secondNumber = Number.parseInt(second1.value);
 
   multiplicarDiv.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
